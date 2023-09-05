42d3437d77d4db700b98233b4819372f85c13313
Mikhail <leonovm143@gmail.com>
2023-09-05 20:16:10 +0600

    Styles for table entries finished

--- src/components/linksList/LinksList.tsx
+++ src/components/linksList/LinksList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './LinksList.module.scss';
 import Icon from 'shared/Icon';
+import QRCode from 'react-qr-code';
 
 export const LinksList = () => {
   return (
@@ -24,12 +25,40 @@ export const LinksList = () => {
             https://www.twitch.tv/quinella_admin
           </a>
           <span className={styles.icon}>
-            <Icon.CopyIcon
-              stroke='#C9CED6'
-              width={24}
-              height={24}
-            />
+            <Icon.CopyIcon stroke='#C9CED6' width={24} height={24} />
           </span>
+        </td>
+        <td>
+          <a
+            href='https://www.twitch.tv/quinella_admin'
+            className={styles.link__short}
+            rel='noreferrer'
+            target='_blank'
+          >
+            https://www.twitch.tv/quinella_admin
+          </a>
+        </td>
+        <td style={{ padding: '8px' }}>
+          <QRCode
+            bgColor='transparent'
+            fgColor={'#C9CED6'}
+            style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
+            value={'https://www.twitch.tv/quinella_admin'}
+          />
+        </td>
+        <td>11313</td>
+        <td>
+          {/* <span className={styles.active}>Active</span>
+          <span className={styles.active__icon}>
+            <Icon.LinkIcon stroke='white' />
+          </span> */}
+          <span className={styles.inactive}>Inactive</span>
+          <span className={styles.inactive__icon}>
+            <Icon.LinkIcon stroke='white' />
+          </span>
+        </td>
+        <td>
+          Oct-10-2023
         </td>
       </tr>
     </table>
